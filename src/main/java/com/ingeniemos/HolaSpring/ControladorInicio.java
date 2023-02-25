e1cbfd51fb981ff9ebe75575fa8a1427e9b0b92d
Daniel Alberto Henao Zapata <danielhenao00@gmail.com>
2023-02-24 22:37:49 -0500

    Hola Mundo

---
+++ src/main/java/com/ingeniemos/HolaSpring/ControladorInicio.java
@@ -0,0 +1,15 @@
+package com.ingeniemos.HolaSpring;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class ControladorInicio {
+    @GetMapping("/")
+    public String inicio() {
+        log.info("Ejecutando el controlador rest");
+        return "Hola Mundo Con Spring LiveReload 12345";
+    }
+}
